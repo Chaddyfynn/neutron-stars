8159144baac4b98429d33ed043cd34fd5cef0627
Chaddyfynn <chaddyfynn@gmail.com>
2023-02-24 11:59:47 +0000

    Pressure and Mass plotting, but not correct values

--- RK4.py
+++ RK4.py
@@ -72,12 +72,12 @@ def eps(radius, state):
 
 
 def grad(radius, state):
+    m, p = state
+    dm_dr = ((4 * np.pi * np.power(radius, 2)) /
+             (M0*c**2)) * np.power((p/K), 1/GAMMA)
+    dp_dr = -1 * (R0/(np.power(radius, 2))) * \
+        (p/K)**(1/GAMMA) * m
 
-    dm_dr = 4 * np.pi * np.power(radius, 2) * \
-        eps(radius, state) / np.power(con.c, 2)
-    dp_dr = -1 * con.G * eps(radius, state) * \
-        state[0] / (np.power(con.c * radius, 2))
-    # print(dp_dr)
     '''
     m, p = state
     dm_dr = ((4 * np.pi * np.power(radius, 2)) /
@@ -101,10 +101,10 @@ M0 = 1.989e30
 R0 = (con.G*M0)/(c**2)
 
 
-#K = np.power(con.hbar, 2) / (15 * np.power(np.pi, 2) *con.m_e) \
+# K = np.power(con.hbar, 2) / (15 * np.power(np.pi, 2) *con.m_e) \
 #     (3 * np.power(np.pi, 2) * Z / (M_N * np.power(con.c, 2) * A))
 # print(K)
-K = 6
+K = 1e-22
 GAMMA = 5/3
 
 times, states = rk4_n_steps(grad, t_0, state_0, 10, 1200)
@@ -112,17 +112,21 @@ print(times)
 print(states)
 
 # Prepare two side by side plots
-fig, [ax1, ax2] = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+fig, [ax1, ax3] = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+ax2 = ax1.twinx()
 
 # Axis 1: Show the different state variables against time
 ax1.set(xlabel="Radius r, meters")
-ax1.plot(times, states[:, 0], label="Mass")
-ax1.plot(times, states[:, 1], linestyle="--", label="Pressure")
+ax1.set(ylabel="Mass, Solar Masses")
+ax2.set(ylabel="Pressure, dyne/cm^2")
+ax1.plot(times, states[:, 0], color="red", label="Mass")
+ax2.plot(times, states[:, 1], linestyle="--", label="Pressure")
 ax1.legend()
+ax2.legend()
 
 # Axis 2: Show the x,y plane
-#ax2.set(xlabel="Radius r, meters", ylabel="Mass, kg")
-#ax2.plot(states[:, 0], states[:, 1], label="Mass 1")
+ax3.set(xlabel="Radius r, meters", ylabel="Mass, kg")
+ax3.plot(states[:, 0], states[:, 1], label="Mass 1")
 
 
 # Show and close the plot
