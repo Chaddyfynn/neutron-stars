e86a4db39be3f1688f2c7db2e10129645b39db90
Chaddyfynn <chaddyfynn@gmail.com>
2023-02-28 12:31:08 +0000

    changes

--- white_dwarfs.py
+++ white_dwarfs.py
@@ -9,30 +9,39 @@ import numpy as np
 import scipy.constants as con
 import RK4 as solve
 
-R_0 = 1 # Start Radius
-STATE_0 = np.array([0, 2.2e22])  # Boundary Conditions at R=R_0 [Mass, Pressure]
-STEP = 1 # Step Size
-NUM = 12_700 # Number of Steps
- 
-c = con.c # Speed of Light, m/s
-M_SUN = 1.989e30 # Mass of sun in kg
-R_SCHW = (con.G*M_SUN)/(c**2) # Solar Schwarzschild Radius
+R_0 = 1  # Start Radius
+# Boundary Conditions at R=R_0 [Mass, Pressure]
+STATE_0 = np.array([0, 2.2e22])
+STEP = 1  # Step Size
+NUM = 12_700  # Number of Steps
 
-K = 1e-29 # Pressure Constant
-GAMMA = 5/3 
+c = con.c  # Speed of Light, m/s
+M_SUN = 1.989e30  # Mass of sun, kg
+R_SCHW = (con.G*M_SUN)/(c**2) * 0.001  # Solar Schwarzschild Radius, km
+
+M_E = 9.1093837e-31  # Electron Mass, kg
+M_N = 1.67492749804-27  # Neutron Mass, kg
+A = 2
+Z = 1
+GAMMA = 5/3
+
+K = 6.821915394e-21  # Pressure Constant
+
 
 def main():
     radii, states = solve.rk4(grad, R_0, STATE_0, STEP, NUM)
     solve.plot(radii, states)
-    
+
+
 def grad(radius, state):
     m, p = state
     dm_dr = ((4 * np.pi * np.power(radius, 2)) /
              (M_SUN*c**2)) * np.power((p/K), 1/GAMMA)
     dp_dr = -1 * (R_SCHW/(np.power(radius, 2))) * \
-        (p/K)**(1/GAMMA) * m
-        
+        np.power((p/K), (1/GAMMA)) * m
+
     return np.array([dm_dr, dp_dr])  # gradient array
 
+
 if __name__ == "__main__":
-   main()
+    main()
