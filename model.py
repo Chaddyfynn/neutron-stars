edf9d0cc87c1c554c5d4cf38dafa3147c535fb80
Chaddyfynn <chaddyfynn@gmail.com>
2023-03-20 02:24:28 +0000

    Protons and Electrons Added

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ import time
 
 # RK4 / Calculator Settings
 R_0 = 0.0001  # Initial Condition Radius, m
-R_F = 45_000  # Final Radius, m
+R_F = 5_000  # Final Radius, m
 R_SPAN = [R_0, R_F]  # Radii Span
 
 # System Settings
@@ -30,7 +30,7 @@ MAX_PRESSURE = 1e33  # Maximum Central Pressure, Pa
 NUM_STEPS = 100  # Number of Iterations (Plot Points on Graph)
 PRESSURE_STEP = (MAX_PRESSURE - MIN_PRESSURE) / NUM_STEPS
 TOLERANCE = 1e15  # Radius Root Finding Tolerance (Changes Meaning According to Root Finding Algorithm)
-LOGARITHMIC = True # Plot and Produce Points Logarithmically? (Boolean)
+LOGARITHMIC = False # Plot and Produce Points Logarithmically? (Boolean)
 if LOGARITHMIC:
     P_EVAL = np.logspace(np.log10(MIN_PRESSURE), np.log10(MAX_PRESSURE), NUM_STEPS) # Logarithmic Points
 else:
@@ -43,13 +43,17 @@ R0 = (con.G*M0)/(con.c**2)  # Solar Schwarzchild Radius, m
 # Thermodynamic Constants
 EPS_N_0 = np.power(con.m_n, 4) * np.power(con.c, 5) / (np.power(np.pi, 2) *
                                                        np.power(con.hbar, 3)) # Neutron Star Energy Density Constant, J m^-3
+EPS_E_0 = np.power(con.m_e, 4) * np.power(con.c, 5) / (np.power(np.pi, 2) *
+                                                       np.power(con.hbar, 3)) # Neutron Star Energy Density Constant, J m^-3
+EPS_P_0 = np.power(con.m_p, 4) * np.power(con.c, 5) / (np.power(np.pi, 2) *
+                                                       np.power(con.hbar, 3)) # Neutron Star Energy Density Constant, J m^-3
 
 # Save and Graph Settings
 FILENAME = "TOV_Neutron_Star_Range"  # Graph and Text File Desired Name
 PLOT_TIME = False  # Plot Function Evaluation Times vs Pressure? (Boolean)
 # Compute for a range of central pressures (True), or one (False)
-FULL_COMPUTATION = True # Compute over central pressure range?
-PLOT_INDIVIDUAL = False  # Create graphs for each central pressure (False)
+FULL_COMPUTATION = False # Compute over central pressure range?
+PLOT_INDIVIDUAL = True  # Create graphs for each central pressure (False)
 CROP = 0  # Left Crop for Full computation, 5e23 for rel
 METADATA = [R_0, MIN_PRESSURE, MAX_PRESSURE, NUM_STEPS] # Desired save metadata
 
@@ -94,13 +98,9 @@ class Newtonian(Star): # Newtonian Sub and Superclass
         return np.array([dm_dr, dp_dr])
 
 
-class FermiModel(Newtonian):
+class PureNeutronFermiModel(Newtonian):
     def __init__(self, central_pressure):
         super().__init__(central_pressure)
-
-    def energy_density(self, state):
-        mass, pressure = state
-        return self.full_energy_density(state)
     
     def fermi_pressure(self, x, pressure):
         # print("Calculating at", pressure)
@@ -113,12 +113,60 @@ class FermiModel(Newtonian):
         return EPS_N_0/8 * ((2 * np.power(x, 3) + x) * np.power(1 +
                                                                 np.power(x, 2), 1/2) - np.arcsinh(x))
 
-
-    def full_energy_density(self, state):
+    def energy_density(self, state):
         mass, pressure = state
         x = sci_opt.bisect(self.fermi_pressure, 150, -150,
                            args=(pressure), maxiter=10000)
         return self.fermi_energy_density(x)
+    
+class ProtonElectronNeutronFermiModel(Newtonian):
+    def __init__(self, central_pressure):
+        super().__init__(central_pressure)
+        self.last_solution = [0, 0, 0]
+        
+    def proton_pressure(self, x):
+        return EPS_P_0/24 * ((2 * np.power(x, 3) - 3 * x) *
+                             np.power(1 + np.power(x, 2), 1/2) + 3 * np.arcsinh(x))
+        
+    def neutron_pressure(self, x):
+        return EPS_N_0/24 * ((2 * np.power(x, 3) - 3 * x) *
+                             np.power(1 + np.power(x, 2), 1/2) + 3 * np.arcsinh(x))
+        
+    def electron_pressure(self, x):
+        return EPS_E_0/24 * ((2 * np.power(x, 3) - 3 * x) *
+                             np.power(1 + np.power(x, 2), 1/2) + 3 * np.arcsinh(x))
+    
+    def fermi_pressure(self, x):
+        return self.proton_pressure(x[0]) + self.neutron_pressure(x[1]) + self.electron_pressure(x[2])
+    
+    def fermi_pressure_calc(self, x, pressure):
+        scalar = self.fermi_pressure(x) - pressure
+        scalar = scalar[0]
+        return np.array([scalar, scalar, scalar])
+    
+    def proton_energy_density(self, x):
+        return EPS_P_0/8 * ((2 * np.power(x, 3) + x) * np.power(1 +
+                                                                np.power(x, 2), 1/2) - np.arcsinh(x))
+        
+    def neutron_energy_density(self, x):
+        return EPS_N_0/8 * ((2 * np.power(x, 3) + x) * np.power(1 +
+                                                                np.power(x, 2), 1/2) - np.arcsinh(x))
+        
+    def electron_energy_density(self, x):
+        return EPS_E_0/8 * ((2 * np.power(x, 3) + x) * np.power(1 +
+                                                                np.power(x, 2), 1/2) - np.arcsinh(x))
+    
+    def energy_density(self, x):
+        return self.proton_energy_density(x[0]) + self.neutron_energy_density(x[1]) + self.electron_energy_density(x[2])
+
+    def energy_density_calc(self, state):
+        mass, pressure = state
+        solution = sci_opt.root(self.fermi_pressure_calc, self.last_solution,
+                           args=(pressure), method='broyden1', jac=False) # broyden1,2, anderson okay but slow, df-sane okay but different to broyden
+        x = solution.x
+        print("Solution Found", x)
+        self.last_solution = x
+        return self.energy_density(x)
 
 
 # Defines the structure of any gas with a Polytropic EoS
@@ -140,7 +188,7 @@ class PolytropeModel(Newtonian):
     # radius = [radius (m)], state = [mass (kg), pressure (Pa)]
 
 
-class TOVModel(FermiModel):
+class TOVModel(PureNeutronFermiModel):
     def __init__(self, central_pressure):
         super().__init__(central_pressure)
         # Code go here
@@ -155,7 +203,29 @@ class TOVModel(FermiModel):
         term_3 = np.power(1 - 2 * con.G * (mass) / (c_2 * radius), -1)
         if term_3 < 0:
             print("Imaginary Solution")
-            term_3 = -term_3
+            return np.array([0, 0])
+        dp_dr = -1 * con.G * e_dens * (mass) / (c_2 * sq_radius) * \
+            (1 + pressure/e_dens) * (1 + (4 * np.pi * cb_radius * pressure)/((mass) * c_2)) * \
+            term_3
+        dm_dr = ((4 * np.pi * sq_radius) * e_dens/((con.c)**2))
+        return np.array([dm_dr, dp_dr])
+    
+class TOVModel2(ProtonElectronNeutronFermiModel):
+    def __init__(self, central_pressure):
+        super().__init__(central_pressure)
+        # Code go here
+
+    def grad(self, radius, state):
+        # print("Calculating Grad at", round(radius, 0), "m")
+        mass, pressure = state
+        sq_radius = np.power(radius, 2)
+        cb_radius = np.power(radius, 3)
+        e_dens = self.energy_density_calc(state)
+        c_2 = np.power(con.c, 2)
+        term_3 = np.power(1 - 2 * con.G * (mass) / (c_2 * radius), -1)
+        if term_3 < 0:
+            print("Imaginary Solution")
+            return np.array([0, 0])
         dp_dr = -1 * con.G * e_dens * (mass) / (c_2 * sq_radius) * \
             (1 + pressure/e_dens) * (1 + (4 * np.pi * cb_radius * pressure)/((mass) * c_2)) * \
             term_3
@@ -165,9 +235,11 @@ class TOVModel(FermiModel):
 
 def solve_individual(body, r_span):
     state_0 = [1e-6, body.p0]  # Initial state at R_0
+    start_time = time.time()
     solution = sci_int.solve_ivp(body.grad, r_span, state_0, method='RK45',
                                  t_eval=None, dense_output=False, events=None,
                                  vectorized=True, args=None, max_step=1000)
+    print("Calculation finished in", round(time.time() - start_time, 2), "s")
     radii = solution['t']
     masses = solution['y'][0]
     pressures = solution['y'][1]
@@ -180,10 +252,14 @@ def root(radii, states, tolerance):
     loc_array = np.where(pressures <= tolerance)
     prime_index = loc_array[0]
     # prime_index = indices[0]
-    radius, mass, pressure = radii[prime_index][0], masses[prime_index][0], pressures[prime_index]
-    print("Root found at", radius /
-          1000, "km and", mass, "M0.")
-    return radius, mass
+    if len(radii[prime_index]) == 0:
+        print("No roots found ...")
+        return 0, 0
+    else:
+        radius, mass, pressure = radii[prime_index][0], masses[prime_index][0], pressures[prime_index]
+        print("Root found at", radius /
+              1000, "km and", mass, "M0.")
+        return radius, mass
 
 
 def rory(radius, state, tolerance):
@@ -228,8 +304,9 @@ if __name__ == "__main__":
     rel_wd_star = PolytropeModel(K_WD_REL, GAMMA_REL, MIN_PRESSURE)
     non_rel_n_star = PolytropeModel(K_N_NON_REL, GAMMA_NON_REL, MIN_PRESSURE)
     full_tov_n_star = TOVModel(MIN_PRESSURE)
-    full_newton_n_star = FermiModel(MIN_PRESSURE)
-    star = full_tov_n_star
+    tov_n_star = TOVModel2(MIN_PRESSURE)
+    full_newton_n_star = PureNeutronFermiModel(MIN_PRESSURE)
+    star = tov_n_star
     if FULL_COMPUTATION:
         radii, masses, pressures = solve_range(
             star, MAX_PRESSURE, PRESSURE_STEP, TOLERANCE, R_SPAN, FILENAME)
